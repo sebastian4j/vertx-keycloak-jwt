6d739d8f8750d1817e2866494e116c66e72b146d
Sebastian Avila A <sebastian4j@gmail.com>
2019-01-29 01:18:06 -0300

    vertx configurado para utilizar keycloak
    configuración inicial de la aplicación, pendiente subir la configuracion del keycloak

---
+++ src/main/java/com/sebastian/vertx/VertxKeycloak.java
@@ -0,0 +1,55 @@
+package com.sebastian.vertx;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.core.Vertx;
+import io.vertx.ext.auth.User;
+import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.OAuth2AuthHandler;
+
+/**
+ * Utilizando keycloak JWT para la autorizacion del acceso a recursos.
+ *
+ * @see <a href="https://vertx.io/">vertx.io</a>
+ * @see <a href="https://vertx.io/docs/">https://vertx.io/docs/</a>
+ * @see <a href= "https://vertx.io/docs/vertx-auth-oauth2/java/">vertx.io/vertx-auth-oauth2/</a>
+ * @see <a href="https://jwt.io/">jwt.io/</a>
+ *
+ *
+ * @author Sebastian Avila A.
+ */
+public class VertxKeycloak {
+  private static final Logger LOGGER = Logger.getLogger(VertxKeycloak.class.getCanonicalName());
+
+  public static void main(String[] args) {
+    final var vertx = Vertx.vertx();
+    ConfigRetriever.create(vertx).getConfig(c -> {
+      final var config = c.result();
+      final var router = Router.router(vertx);
+      final var oa =
+          OAuth2AuthHandler.create(KeycloakAuth.create(vertx, config.getJsonObject("keycloak")),
+              config.getString("callback"));
+      oa.setupCallback(router.get("/callback"));
+      router.route("/resources/*").handler(oa);
+      router.route("/resources/secure").handler(rc -> {
+        final User s = rc.user();
+        s.isAuthorized(config.getString("rol-esperado"), res -> {
+          if (res.succeeded()) {
+            if (res.result()) {
+              LOGGER.info("autorizado =)");
+            } else {
+              LOGGER.info("NO autorizado =(");
+            }
+          } else {
+            LOGGER.log(Level.WARNING, "error al autorizar", res.cause());
+          }
+        });
+        LOGGER.log(Level.INFO, "principal {0}", s.principal());
+        rc.response().end("hola desde el recurso protegido");
+      });
+      vertx.createHttpServer().requestHandler(router).listen(config.getInteger("puerto"));
+    });
+  }
+}
